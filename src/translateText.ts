0b9a6c67f36e61e1d55a0b090d8e6a66b6690a70
Lisa Stedman-Falls <lstedmanfalls@gmail.com>
2024-01-01 13:26:21 -0700

    rename getPdfText function

--- src/translateText.ts
+++ src/translateText.ts
@@ -1,7 +1,7 @@
 import { extractPdfText } from './extractPdfText';
 import { transformText } from './transformText';
 
-const getTextFromPdf = async (start: number = 1, end?: number) => {
+const getPdfText = async (start: number = 1, end?: number) => {
   const rawTextPages = await extractPdfText(start, end);
   const transformedTextPages = transformText(rawTextPages);
   console.log(transformedTextPages);
@@ -9,4 +9,4 @@ const getTextFromPdf = async (start: number = 1, end?: number) => {
 };
 
 // Provide start, or start and end, to get specific pages. Pass nothing to get entire document
-getTextFromPdf();
+getPdfText();
