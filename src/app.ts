a181691e93b7f560150e239e8f3977a3e685d3ea
Lisa Stedman-Falls <lstedmanfalls@gmail.com>
2024-01-07 10:50:22 -0700

    cleanup app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import express from 'express';
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 import main from './routers/main';
+
 const app = express();
 const port = 3000;
 app.use('/', main);
